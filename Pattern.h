b7fd99ea9b98821d8c71a585537aa3abb7fb98c6
Jordan Adania <45571854+jordanadania@users.noreply.github.com>
2020-07-11 17:29:00 -0400

    Update Pattern.h
    i don't know specifically what i changed, but this is my current version

--- Pattern.h
+++ Pattern.h
@@ -1,54 +1,60 @@
 void fadeWhites(uint8_t value){
   for(int i=1; i<NUM_LEDS-1; ++i)                                 
-    if((leds[i].r == leds[i].g && leds[i].g == leds[i].b)
-    &&(max(max(leds[i].r, leds[i].g), leds[i].b) > 32
-    &&(leds[i].r > max(max(leds[i-1].r, leds[i-1].g), leds[i-1].b)
-    ||(leds[i-1].r== leds[i-1].g && leds[i-1].g== leds[i-1].b))
-    &&(leds[i].r > max(max(leds[i+1].r, leds[i+1].g), leds[i+1].b)
-    ||(leds[i+1].r== leds[i+1].g && leds[i+1].g== leds[i+1].b))
-    || leds[i].r > 96))
-       leds[i].fadeToBlackBy(value);
+    if(
+        (ledz[i].r==ledz[i].g && ledz[i].g==ledz[i].b           // if LED is white AND
+        &&(max(max(ledz[i].r, ledz[i].g), ledz[i].b) > 32))               // is at least 32 brightness
+    
+      &&((ledz[i].r > max(max(ledz[i-1].r, ledz[i-1].g), ledz[i-1].b)  // AND LED is brighter than left neighbor OR
+        ||(ledz[i-1].r== ledz[i-1].g && ledz[i-1].g== ledz[i-1].b))     // left neighbor is WHITE
+        &&(ledz[i].r > max(max(ledz[i+1].r, ledz[i+1].g), ledz[i+1].b)  // AND led is brighter than right neighbor OR
+          ||(ledz[i+1].r== ledz[i+1].g && ledz[i+1].g== ledz[i+1].b)))     // 
+    
+    || ledz[i].r > 96)
+       ledz[i].fadeToBlackBy(value);
 }
 void freshWhites(){
   for(int i=0; i<NUM_LEDS; ++i)
-    if(leds[i].r==leds[i].g && leds[i].g==leds[i].b)
-      leds[i] = CRGB::White;
+    if(ledz[i].r==ledz[i].g && ledz[i].g==ledz[i].b)
+      ledz[i] = CRGB::White;
 }
 void goGray(){
   for(uint16_t i=0; i<NUM_LEDS; ++i)
-    if(leds[i]){
-      newHue = max(max(leds[i].r, leds[i].g), leds[i].b);
-      leds[i] = CRGB(newHue,newHue,newHue);
+    if(ledz[i]){
+      newHue = max(max(ledz[i].r, ledz[i].g), ledz[i].b);
+      ledz[i] = CRGB(newHue,newHue,newHue);
     }
 }
 void push(uint16_t k){
     for(uint16_t i=k; i>0; --i)
-      leds[i] = leds[i-1];
+      ledz[i] = ledz[i-1];
 }
 void pull(uint16_t k){
     for(uint16_t i=0; i<k; ++i)
-      leds[i] = leds[i+1];
+      ledz[i] = ledz[i+1];
 }
 void mirror(uint16_t k){
     for(int i=0; i<k; ++i)
-      leds[k-i]=leds[i];
+      ledz[k-i]=ledz[i];
 }
 void copyIt(byte k){
-  memcpy(leds+NUM_LEDS/k, leds,
-    modus[k]<1?(NUM_LEDS*sizeof(CRGB)*(k-1)/k):
- (NUM_LEDS*sizeof(CRGB)*(k-1)/k)+sizeof(CRGB));
+  memcpy((s=="New Flow"?ledz:leds)+NUM_LEDS/k,
+         (s=="New Flow"?ledz:leds),NUM_LEDS*sizeof(CRGB)*(k-1)/k);
 }
+void goUp()  { mrs = !mrs;             morphs+=13; }
+void goDown(){ mrs = !mrs; morphs!=25? morphs-=12:morphs=0; }
+void morph() { mrs? goUp():goDown(); }
+
 uint8_t getAverage32(uint8_t in[]){
   uint16_t out = 0;
   for(int i=0; i<32; ++i)
     out+=in[i];
-  return out/32;
+  return out>>5;
 }
 uint8_t getAverage255(uint8_t in[]){
   uint16_t out = 0;
   for(int i=0; i<SAMPLES; ++i)
     out+=in[i];
-  return out/320;
+  return out/SAMPLES;
 }
 uint8_t getMax255(uint8_t in[]){
   uint8_t out = 0;
@@ -76,81 +82,91 @@ uint8_t getMaxTreb(uint8_t in[]){
     }
   return out;
 }
-uint16_t idx;
-uint8_t bri[7], dfm[7], dfa[7],
-        smp[7][SAMPLES],dif[7][SAMPLES], 
-        bass[7], treb[7];
-void saveBands(){
-  for(byte b=0; b<7; ++b)
-    smp[b][idx] = pow(spectrumByte[b]>>4,2);
-  for(byte b=0; b<7; ++b)
-    dif[b][idx] = abs(smp[b][idx+1]-smp[b][idx]);
+uint8_t bri[NUM_BANDS], dfm[NUM_BANDS], dfa[NUM_BANDS],
+        smp[NUM_BANDS][SAMPLES],dif[NUM_BANDS][SAMPLES], 
+        bass[NUM_BANDS], treb[NUM_BANDS];
+void saveBands(uint16_t in){
+  for(byte b=0; b<NUM_BANDS; ++b)
+    smp[b][in] = pow(spectrumByte[b]>>4,2);
+  for(byte b=0; b<NUM_BANDS; ++b)
+    dif[b][in] = abs(smp[b][in+1]-smp[b][in]);
 }
 void analyzeBands(){
-  for(byte b=0; b<7; ++b)
+  // Get Average
+  for(byte b=0; b<NUM_BANDS; ++b)
     bri[b] = getAverage255(smp[b]);
-  for(byte b=0; b<7; ++b)
+  // Get Average Difference
+  for(byte b=0; b<NUM_BANDS; ++b)
     dfa[b] = getAverage255(dif[b]);
-  for(byte b=0; b<7; ++b)
+  // Get Biggest Difference
+  for(byte b=0; b<NUM_BANDS; ++b)
     dfm[b] = getMax255(dif[b]);
+  // Bass Analysis
   for(byte b=0; b<7; ++b){
-    bass[b] = bri[b]>dfm[b]? 0: dfm[b]-bri[b];
-    if(b<3&&bassConf!=b)
-      bass[b] *= 5>>2;
+    bass[b] = bri[b]>>1>dfm[b]? 0: dfm[b]-bri[b]>>1;
+    if(b<3&&bassBand!=b)
+      bass[b] *= 9>>3;
   }
-  bass[bassConf] *= 5>>2;
+  bass[bassBand] *= 5>>2;
   // Treb Analysis
-  for(byte b=0; b<7; ++b)
-    treb[b] = (dfm[b]<<2/3+bri[b]>>1)>>2;
-  treb[trebConf] *= 9>>3;
-  if(trebConf!=6)
-    treb[6] *= 9>>3;
+  for(byte b=0; b<7; ++b){
+    treb[b] = (bri[b]+dfm[b]>>1)>>1;
+    if(b>2&&trebBand!=b)
+      treb[b] *= 9>>3;
+  }
+  treb[trebBand] *= 5>>2;
 
-  trebConf = trebBand = getMaxTreb(treb);
-  bassConf = bassBand = getMaxBass(bass);
+  trebBand = getMaxTreb(treb);
+  bassBand = getMaxBass(bass);
 }
-
-void newFlow(){
-  readAudio();
-    EVERY_N_SECONDS(2){
-      if(spectrumAvg>48)
-        analyzeBands();
-      else 
-        bassConf=bassBand=0,
-        trebConf=trebBand=6;
-    }
-    static uint8_t rgbRate, huey;
-    uint8_t bri, rate;
-    bool bb = true, bt = bb;
-    
+void newFlow(){ audio = true;
+  static uint16_t idx;
+  uint8_t bri, rate;
+  bool bb = true, bt = true;
+  EVERY_N_SECONDS(8){ if(mor) morph(); }
+  EVERY_N_SECONDS(2){ if(spectrumAvg>48) analyzeBands(); else bassBand=0,trebBand=6; }
     fadeWhites(16);
-      bool zero = speed==0? true: false;
-    uint16_t  k = edges[speed];
-    uint16_t hk = k>>1;
-    uint16_t ok = inv? (zero?k:hk): 0;
-    
-    saveBands();
-    bri = smp[trebBand][idx];
-    idx = idx==SAMPLES-1? 0: idx+1;
-    
-    rate = (getAverage32(smp[trebBand])>>5);
-    if(rgbRate>rate+1) rgbRate-=rate;
-    
-    if(rgbRate>3)      rgbRate-=rgbRate/4;
-    if(rgbRate>0)      rgbRate--;
-    
-    inv? pull(zero? k: hk): push(zero? k: hk);
-    
-    if(beatDetect()&&spectrumAvg>96){
+   // if(tfd) fadeColors(32);
+    if(mor) speed = morphs;
+        bool zero = speed==0? true: false;
+      uint16_t  k = edges[speed];
+      uint16_t hk = k>>1;
+      uint16_t ok = inv? (zero?k:hk): 0;
+  saveBands(idx);
+   bri = smp[trebBand][idx];
+   idx = idx==SAMPLES-1? 0: idx+1;
+  rate = getAverage32(smp[trebBand])>>5;
+  if(   rate>1)        --rate;
+  if(rgbRate>3) rgbRate-=rgbRate>>2;
+  if(rgbRate>0)        --rgbRate;
+  if(!bmd&&!sv2){
+    memcpy(ledz, last, NUM_LEDS*3);
+    sv2 = !sv2;
+  }
+  inv? pull(zero? k: hk): push(zero? k: hk);
+  if(beatDetect()){
+    if(!bmd){
+      if(sv2){
+        memcpy(last, ledz, NUM_LEDS*sizeof(CRGB));
+        fmd?goGray():fill_solid(ledz,NUM_LEDS,CRGB::White);
+        sv2 = !sv2;
+      }
+    } else{
       freshWhites();
-      leds[ok] = CRGB::White;
-      if(rate>0) leds[ok==0? ok+1: ok-1] = CRGB::White;              // IF 12 of 32 brightness thresholds were
-    } else{ // beatDetect()
-      if(trebDetect()&&spectrumAvg>64){ rgbRate+=16; }                            // "trebDetect" increases rgbRate by the most
-      dir? huey+=rgbRate: huey-=rgbRate;    // webapp btn-Hue Speed decided adjustment
-      leds[ok] = ColorFromPalette(RainbowColors_p, huey, bri, LINEARBLEND);   // webapp btn-Palette Mode
-    } // beatDetect()
-    if(bri==0) leds[ok] = CRGB(0,1,0);                        // Never let a pixel be black
-    if(!zero){ mirror(k); copyIt(speed*NUM_STRIPS); }                  // Mirror from Index // copy mirrored section speed*3-1 times
-  //}
+      if(!hup){ rgbRate+=18; 
+        hmd?huey=gHue:(dir?huey+=rgbRate:huey-=rgbRate);
+      }
+      ledz[ok] = CRGB::White;
+      if(rate>0)
+        ledz[ok==0? ok+1: ok-1] = CRGB::White;
+    }
+  } else{ // beatDetect()
+    if(trebDetect()&&hup){ rgbRate+=9; }
+    if(hup)
+      hmd? huey=gHue: dir? huey+=rgbRate: huey-=rgbRate;
+    ledz[ok] = ColorFromPalette(pmd? palettes[currPalIdx]:
+                gCurrentPalette, huey, bri, LINEARBLEND);
+  } // beatDetect()
+  if(bri==0) ledz[ok] = CRGB(0,1,0);
+  if(!zero){ mirror(k); copyIt(speed*3); }
 }
