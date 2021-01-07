4ffc1540831fbc2c09a9585064ae3c27423c03e1
Jordan Adania <45571854+jordanadania@users.noreply.github.com>
2021-01-07 12:07:13 -0500

    Update Pattern.h

--- Pattern.h
+++ Pattern.h
@@ -34,12 +34,11 @@ void mirror(uint16_t k){
       leds[k-i]=leds[i];
 }
 void copyIt(byte k){
-  memcpy((s=="New Flow"?leds:leds)+NUM_LEDS/k,
-         (s=="New Flow"?leds:leds),NUM_LEDS*sizeof(CRGB)*(k-1)/k);
+  memcpy(leds+NUM_LEDS/k, leds,
+    modus[k]<1?
+      NUM_LEDS/k*sizeof(CRGB):
+      NUM_LEDS/k*sizeof(CRGB)+sizeof(CRGB));
 }
-void goUp()  { mrs = !mrs;             morphs+=13; }
-void goDown(){ mrs = !mrs; morphs!=25? morphs-=12:morphs=0; }
-void morph() { mrs? goUp():goDown(); }
 
 uint8_t getAverage32(uint8_t in[]){
   uint16_t out = 0;
@@ -60,8 +59,8 @@ uint8_t getMax255(uint8_t in[]){
   return out;
 }
 uint8_t getMaxBass(uint8_t in[]){
-  uint8_t out = 0, mx = 0;
-  for(int i=6; i>0; --i)
+  uint8_t out = 0, mx = in[out];
+  for(int i=out+1; i<=6; ++i)
     if(mx<in[i]){
       mx = in[i];
       out = i;
@@ -69,78 +68,85 @@ uint8_t getMaxBass(uint8_t in[]){
   return out;
 }
 uint8_t getMaxTreb(uint8_t in[]){
-  uint8_t out = 0, mx = 0;
-  if(bassBand==0)
-    mx = 1;
-  for(int i=6; i>0; --i)
-    if(mx<in[i]&&i!=bassBand){
+  uint8_t out = 6, mx = in[out];
+  for(int i=out-1; i>=0; --i)
+    if(mx<in[i]){
       mx = in[i];
       out = i;
     }
   return out;
 }
+uint16_t idx;
 uint8_t bri[NUM_BANDS], dfm[NUM_BANDS], dfa[NUM_BANDS],
         smp[NUM_BANDS][SAMPLES],dif[NUM_BANDS][SAMPLES], 
         bass[NUM_BANDS], treb[NUM_BANDS];
 void saveBands(uint16_t in){
-  for(byte b=0; b<NUM_BANDS; ++b)
-    smp[b][in] = pow(spectrumByte[b]>>4,2);
+  for(byte b=0; b<7; ++b)
+    //smp[b][idx] = pow(spectrumByte[b]>>4,2);
+    smp[b][idx] = spectrumByte[b];
   for(byte b=0; b<NUM_BANDS; ++b)
     dif[b][in] = abs(smp[b][in+1]-smp[b][in]);
 }
 void analyzeBands(){
   
+  bool bb = true;
+  bool bt = true;
+  byte newBass = 0;
+  byte newTreb = 6;
+  
   // Get Average
   for(byte b=0; b<NUM_BANDS; ++b)
     bri[b] = getAverage255(smp[b]);
-    
   // Get Average Difference
   for(byte b=0; b<NUM_BANDS; ++b)
     dfa[b] = getAverage255(dif[b]);
-    
   // Get Biggest Difference
   for(byte b=0; b<NUM_BANDS; ++b)
     dfm[b] = getMax255(dif[b]);
     
   // Bass Analysis
-  for(byte b=0; b<7; ++b){
+  for(byte b=0; b<NUM_BANDS; ++b)
     bass[b] = bri[b]>dfm[b]? 0: dfm[b]-bri[b];
-    //if(b<3&&bassBand!=b)
-    //  bass[b] *= 9>>3;
-  }
-  bass[bassBand] *= 9>>3;
-  
   // Treb Analysis
-  for(byte b=0; b<7; ++b){
-    treb[b] = dfm[b];
-   // if(b>2)
-   //  treb[b] *= 9>>3;
-  }
-  // Band Assignment
-  trebBand = getMaxTreb(treb);
-  bassBand = getMaxBass(bass);
+  for(byte b=0; b<NUM_BANDS; ++b)
+    treb[b] = dfa[b]>>2+bri[b]>>3+dfm[b]>>1;
+    
+  // Weighted Current Band
+  //bass[bassBand] *= 9>>3;
+  //treb[trebBand] *= 9>>3;
   
+  // Weighted Band
+  bass[0] *= 9>>3;
+  treb[6] *= 9>>3;
+  
+  // Band Assignment
+  newTreb = getMaxTreb(treb);
+  newBass = getMaxBass(bass);
+
+  // 2 Strikes Against The Band
+ /* if(bt&&(newTreb!=trebBand)) bt = false;
+  else if(!bt&&newTreb!=trebBand){
+    trebBand = newTreb;
+    bt = true;
+  }*/
+  if(bb&&(newBass!=bassBand)) bb = false;
+  else if(!bb&&newBass!=bassBand){
+    bassBand = newBass;
+    bb = true;
+  }
 }
 void newFlow(){ audio = true;
   static uint16_t idx;
   uint8_t bri, rate;
-  bool bb = true, bt = true;
-  EVERY_N_SECONDS(8){ if(mor) morph(); }
-  //EVERY_N_SECONDS(2){ if(spectrumAvg>48) analyzeBands(); else bassBand=0,trebBand=6; }
-  EVERY_N_SECONDS(2){ analyzeBands(); }
-    fadeWhites(16);
-   // if(tfd) fadeColors(32);
-    if(mor) speed = morphs;
-        bool zero = speed==0? true: false;
-      uint16_t  k = edges[speed];
-      uint16_t hk = k>>1;
-      uint16_t ok = inv? (zero?k:hk): 0;
+  EVERY_N_SECONDS(2){ if(spectrumAvg>32) analyzeBands(); else bassBand=0,trebBand=6; }
+  fadeWhites(16);
+    bool zero = speed==0? true: false;
+  uint16_t  k = edges[speed];
+  uint16_t hk = k>>1;
+  uint16_t ok = inv? (zero?k:hk): 0;
   saveBands(idx);
-   bri = smp[trebBand][idx];
-   idx = idx==SAMPLES-1? 0: idx+1;
-  //rate = getAverage32(smp[trebBand])>>4;
-  //if(   rate>1)        --rate;
-  //              rgbRate+=rate;
+  bri = smp[trebBand][idx];
+  idx = idx==SAMPLES-1? 0: idx+1;
   if(rgbRate>3) rgbRate-=rgbRate>>2;
   if(rgbRate>0)        --rgbRate;
   if(!bmd&&!sv2){
@@ -157,7 +163,7 @@ void newFlow(){ audio = true;
       }
     } else{
       freshWhites();
-      if(!hup){ rgbRate+=16; 
+      if(!hup){ rgbRate+=16;
         hmd?huey=gHue:(dir?huey+=rgbRate:huey-=rgbRate);
       }
       leds[ok] = CRGB::White;
@@ -172,5 +178,6 @@ void newFlow(){ audio = true;
                 gCurrentPalette, huey, bri, LINEARBLEND);
   } // beatDetect()
   if(bri==0) leds[ok] = CRGB(0,1,0);
-  if(!zero){ mirror(k); copyIt(speed*3); }
+  if(!zero){ mirror(k); copyIt(speed*NUM_STRIPS); }
+  //rgbRate-=rate;
 }
