8737ad1261225706c3ad858f6f6f5a4ca9ac6c5d
Jordan Adania <45571854+jordanadania@users.noreply.github.com>
2023-10-03 11:30:47 -0400

    Delete OtA.h
    remove non-pattern code

--- OtA.h
+++
@@ -1,8 +0,0 @@
-WiFi.mode(WIFI_STA);
-WiFi.begin(ssid, password); 
-while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-  Serial.println("Connection Failed! Rebooting...");
-  delay(5000);
-  ESP.restart();
-}
-ArduinoOTA.begin();
